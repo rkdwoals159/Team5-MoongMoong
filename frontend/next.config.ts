e6167b8d9f1f57de7316dd372d15dbdbff23c5f5
yyoonngg <127941824+yyoonngg@users.noreply.github.com>
2026-01-22 19:03:19 +0900

    [CHORE] svgr 라이브러리 설치 및 세팅 (#35)
    Signed-off-by: yyoonngg <127941824+yyoonngg@users.noreply.github.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
+import type { Configuration, RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+
+  webpack(config: Configuration) {
+    const rules = config.module?.rules ?? [];
+
+    const fileLoaderRule = rules.find((rule): rule is RuleSetRule => {
+      if (!rule || typeof rule === "string") return false;
+      return rule.test instanceof RegExp && rule.test.test(".svg");
+    });
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: { and: [/\.(js|ts)x?$/] },
+        use: ["@svgr/webpack"],
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
