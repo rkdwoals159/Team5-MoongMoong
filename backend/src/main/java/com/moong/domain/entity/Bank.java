dfa3974c9d1b8d6bee508164b61941753454c97c
yeonjinJoo <102257328+yeonjinJoo@users.noreply.github.com>
2026-01-28 16:00:10 +0900

    [FEAT] Bank & Coin Entity 설정 (#113)

---
+++ backend/src/main/java/com/moong/domain/entity/Bank.java
@@ -0,0 +1,38 @@
+package com.moong.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "bank",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_bank_group",
+                        columnNames = {"group_id"}
+                )
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Bank extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private PetGroup petGroup;
+
+    private long targetAmount;
+
+    private long currentAmount;
+
+}
---
+++ backend/src/main/java/com/moong/domain/entity/Coin.java
@@ -0,0 +1,33 @@
+package com.moong.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "coin")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Coin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Bank bank;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Crew crew;
+
+    private long amount;
+
+}
